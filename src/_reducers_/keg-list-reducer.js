5bbfbed548e195e9b44df3318337ddfeed1867a8
Tyson Lackey <lackeyt90@gmail.com>
2020-09-18 16:09:01 -0700

    update keg list reducer object for add keg

--- src/_reducers_/keg-list-reducer.js
+++ src/_reducers_/keg-list-reducer.js
@@ -1,3 +1,17 @@
 export default (state = {}, action) => {
-  return state;
+  const{name, brand, price, alcoholContent, pintsRemaining}
+  switch(action.type){
+    case 'ADD_KEG':
+    return Object.assing({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintsRemaining: pintsRemaining
+      }
+    });
+  default:
+    return state;
+  }
 };
